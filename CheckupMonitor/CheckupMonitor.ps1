fe90e05f75a61444b05570e2ae6ca6d097c72f67
mcarmeanATD <mcarmean@td.org>
2015-05-21 16:37:00 -0400

    HTML added
    CheckupMonitor now spits out an html file (partial)

--- CheckupMonitor/CheckupMonitor.ps1
+++ CheckupMonitor/CheckupMonitor.ps1
@@ -3,8 +3,8 @@ Import-Module PSWorkflowUtility
 
 function Get-ServerState {
 	[CmdletBinding()]
-	param(
-		[Parameter(Position=0, Mandatory=$true)]
+	param (
+		[Parameter(Position = 0, Mandatory = $true)]
 		[System.String[]]$ComputerName,
 		[Parameter(Position = 1, Mandatory = $true)]
 		[System.String[]]$Service
@@ -15,61 +15,82 @@ function Get-ServerState {
 	process {
 		foreach ($c in $ComputerName) {
 			
-				$ping = Test-Connection -ComputerName $c -Quiet
+			$ping = Test-Connection -ComputerName $c -Quiet
 			if ("$ping" -eq "False") {
-					#Write-Host "Ping Failed"
-					break
-				}
-				else {
-					# Querying the remote server for config and service info
-					$sess = New-PSSession -ComputerName $c
-					#Write-Host "New session $sess created."
-					
-					$ipInfo = Invoke-Command -Session $sess -ScriptBlock { Get-WmiObject Win32_NetworkAdapterConfiguration | Where { $_.IPAddress.length -gt 0 } }
-					$ipV4 = [string]$ipInfo.IPAddress[0]
-					#Write-Host "IPv4 info is: $ipv4"
-					
-					$timeInfo = Invoke-Command -Session $sess -ScriptBlock { Get-Date }
-					$time = $timeInfo.ToShortTimeString()
-					#Write-Host "Time info: $time"
-					
-					# Collect service information
-					$serviceTable = @{ }
-					#Write-Host "Empty service table created."
-					
-					foreach ($s in $Service) {
-						$serviceObject = Get-Service $s -ComputerName $c
-						#Write-Host "Service object created $s"
-						$serviceTable.Add($serviceObject.Name, $serviceObject.Status)
-						#Write-Host "Service info added to serviceTable"
-					}
-				
-					$properties = @{
-						Name = $c
-						IP = $ipV4
-						Time = $time
-						Service = $serviceTable
-					}
-				
-					New-Object -TypeName PSObject -Property $properties
-					 
-				}
+				#Write-Host "Ping Failed"
+				break
 			}
+			else {
+				# Querying the remote server for config and service info
+				$sess = New-PSSession -ComputerName $c
+				#Write-Host "New session $sess created."
+				
+				$ipInfo = Invoke-Command -Session $sess -ScriptBlock { Get-WmiObject Win32_NetworkAdapterConfiguration | Where { $_.IPAddress.length -gt 0 } }
+				$ipV4 = [string]$ipInfo.IPAddress[0]
+				#Write-Host "IPv4 info is: $ipv4"
+				
+				$timeInfo = Invoke-Command -Session $sess -ScriptBlock { Get-Date }
+				$time = $timeInfo.ToShortTimeString()
+				#Write-Host "Time info: $time"
+				
+				# Collect service information
+				$serviceTable = @{ }
+				#Write-Host "Empty service table created."
+				
+				foreach ($s in $Service) {
+					$serviceObject = Get-Service $s -ComputerName $c
+					#Write-Host "Service object created $s"
+					$serviceTable.Add($serviceObject.Name, $serviceObject.Status)
+					#Write-Host "Service info added to serviceTable"
+				}
+				
+				$properties = @{
+					Name = $c
+					IP = $ipV4
+					Time = $time
+					Service = $serviceTable
+				}
+				
+				New-Object -TypeName PSObject -Property $properties
+				
+			}
+		}
 	}
 	
-	#end {
-#		try {
-#		}
-#		catch {
-#		}
-#}
+	end {
+		Get-PSSession | Remove-PSSession
+	}
 }
+
+$HTMLHead = @"
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Checkup</title>
+<script>
+// Clears cached page/reloads browser every 5 seconds
+`$(document).ready(function () {
+	setInterval(function () { cache_clear() }, 5000);
+});
+
+function cache_clear() {
+	window.location.reload(true);
+}
+
+</script>
+<link href="myCSS.css" rel="stylesheet" type="text/css">
+
+</head>
+"@
+
+Out-File -FilePath \\1476-mcarmean\c`$\users\mcarmean\documents\github\powershell\CheckupMonitor\sites\index.html -InputObject $HTMLHead
 
 $Computers = @("astd-hq-dfs03","1476-mcarmean","astd-hq-dc04")
 foreach ($i in $Computers) {
 	$CurrentComputer = Get-ServerState $i spooler, defragsvc, gpsvc
 	Write-Output $CurrentComputer.Name, $CurrentComputer.Time, $CurrentComputer.IP
 	foreach ($s in $CurrentComputer.Service.GetEnumerator()) {
-		Write-Output $s.Name,$s.Value 	
+		Write-Output $s.Name $s.Value 	
 	}
 }
